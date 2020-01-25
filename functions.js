d5352a76f8fe74f5e28edde01ff103f86282bcd8
virajrajopadhye <virajrajopadhye@gmail.com>
2020-01-24 22:30:40 -0500

    Adding unfinished website

---
+++ functions.js
@@ -0,0 +1,16 @@
+var i = 0;
+var txt = 'Hi, my name is LLAMANATOR. I am a domesticated South American camelid';
+var speed = 80;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
+
+
---
+++ quiz.js
@@ -0,0 +1,140 @@
+function myQuiz(){
+
+var myQuestions = [
+    {
+        question: "Who is my favourite president?",
+        answers: {
+            a: 'Barack O. Llama',
+            b: 'Willama McKinley',
+            c: 'Abraham Llamacoln'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is my favourite drink?",
+        answers: {
+            a: 'Long Island Ice Tea',
+            b: 'Llamon Soda',
+            c: 'Llamanade'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Which is my favourite car?",
+        answers: {
+            a: 'Llamcia',
+            b: 'Llamousine',
+            c: 'Llamabogini'
+        },
+        correctAnswer: 'c'
+    },
+   {
+        question: "Favourite Celebrity?",
+        answers: {
+            a: 'Llamanardo DiCaprio',
+            b: 'Kendrick Llama',
+            c: 'Llama Nesson'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Favourite Sentence?",
+        answers: {
+            a: 'Llama Mia',
+            b: 'No probllama',
+            c: 'Shama Llama Ding Dong'
+        },
+        correctAnswer: 'b'
+    }
+    
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+}
+
+}
